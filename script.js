e9a5119ba66097ce406b44cd1b85bf02a485e65f
LennerJay <lennersoliano@gmail.com>
2023-02-26 21:50:21 +0800

    last commit

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
 let inputEl = document.querySelector("#input-autocomplete");
 let data = ["Red", "Orange", "Blue", "Pink","Rainbow","pinkish","blueshet "];
 autoComplete(inputEl, data);
-
+var index = 0;
 
 function autoComplete(inputEl, data = []){
-    var index = -1;
-     var liSelected;
+
     inputEl.addEventListener("keyup",function(e){
         const li = document.querySelectorAll("li");
-        if(index >= li.length) index =0;
         if(e.which == 40){//arrowdown
-            if(index < li.length){
-                if(liSelected){
-                    li[index].classList.remove(liSelected);
-                    index++;   
-                    li[index].classList.add(liSelected);
-                }else{
-                    liSelected = "selected"
-                    index++;  
-                    li[index].classList.add(liSelected);
-                }
-                return;
-            }else{
-                index = 0;
-                li[index].classList.add(liSelected);
-            }
+            addActive(li)
+            console.log(index);
+            index++;
             return;
-            
         }else if(e.which == 38){//arrowup
-            index--
-            li[index].className ="selected";
+            console.log(index);
+            index--;
+            addActive(li)
            return;
         }else if(e.which == 13){//enter
             inputEl.value = li[0].textContent;
             document.querySelector("#list-autocomplete").remove();
             return;
+        }else if(e.which == 8){
+            index = 0;
+            console.log(index);
         }
-        
         let searchedData = data.filter(item => item.toLowerCase().indexOf(inputEl.value.toLowerCase()) >= 0 && inputEl.value != "");
         if(searchedData.length > 0){
             if(document.querySelector("#list-autocomplete")){
@@ -76,3 +64,16 @@ function insertAfter(newNode, refNode ){
     refNode.parentNode.insertBefore(newNode, refNode.nextSibling);
 }
 
+function removeActive(li) {
+    for (var i = 0; i < li.length; i++) {
+        li[i].classList.remove("selected");
+    }
+}
+ function addActive(li) {
+    if (!li) return false;
+    removeActive(li);
+    if (index >= li.length) index = 0;
+    if (index < 0) index = (li.length - 1);
+    /*add class "autocomplete-active":*/
+    li[index].classList.add("selected");
+  }
